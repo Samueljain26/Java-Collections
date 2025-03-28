1dec59a3cf833341b616e1c94708caab7fa2339d
Samueljain26 <samueljain77@gmail.com>
2025-03-28 10:56:36 +0530

    [Samuel] ADD:reverse a linked list.

---
+++ src/main/java/com/bridgelabz/list/ReverseList.java
@@ -0,0 +1,25 @@
+package com.bridgelabz.list;
+
+import java.sql.SQLOutput;
+import java.util.*;
+
+public class ReverseList {
+    public static void main(String[] args) {
+        List<Integer> ls = createList();
+        System.out.print("Elements of list: "+ ls);
+        Collections.reverse(ls);
+        System.out.print("\nReverse list: "+ ls);
+    }
+    static List<Integer> createList(){
+        Scanner sc = new Scanner(System.in);
+        List<Integer> ls = new LinkedList<>();
+        System.out.println("Enter the elements in list || enter 0 to exit: ");
+        int n =sc.nextInt();
+
+        while(n!=0){
+            ls.add(n);
+            n=sc.nextInt();
+        }
+        return ls;
+    }
+}
