a7a3a5031c16c3e9107b32b416c22ebcf0a94f19
Samueljain26 <samueljain77@gmail.com>
2025-03-28 14:00:33 +0530

    [Samuel] ADD: checking if 2 sets are equal.

---
+++ src/main/java/com/bridgelabz/set/CheckEqualSet.java
@@ -0,0 +1,41 @@
+package com.bridgelabz.set;
+
+import java.util.*;
+
+public class CheckEqualSet {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        Set<Integer> set1 = getSet(sc, "Set 1");
+        Set<Integer> set2 = getSet(sc, "Set 2");
+
+        boolean areEqual = set1.equals(set2);
+        System.out.println("Set 1: " + set1);
+        System.out.println("Set 2: " + set2);
+        System.out.println("Are sets equal? " + areEqual);
+
+        sc.close();
+    }
+
+    // Method to get a set from user input
+    static Set<Integer> getSet(Scanner sc, String setName) {
+        System.out.println("Enter elements for " + setName + " (Enter 0 to stop):");
+        Set<Integer> set = new HashSet<>();
+        while (true) {
+            int n = sc.nextInt();
+            if (n == 0) break;
+            set.add(n);
+        }
+        return set;
+    }
+}
+/*Enter elements for Set 1 (Enter 0 to stop):
+1 2 3 4
+0
+Enter elements for Set 2 (Enter 0 to stop):
+1 2 3 4
+0
+Set 1: [1, 2, 3, 4]
+Set 2: [1, 2, 3, 4]
+Are sets equal? true
+ */
