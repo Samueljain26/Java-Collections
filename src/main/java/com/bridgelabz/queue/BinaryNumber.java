282f1be03426d7f151b3fbb40ad6768853c925d6
Samueljain26 <samueljain77@gmail.com>
2025-03-28 15:29:01 +0530

    [Samuel] ADD: Binary numbers using queue.

---
+++ src/main/java/com/bridgelabz/queue/BinaryNumber.java
@@ -0,0 +1,33 @@
+package com.bridgelabz.queue;
+
+import java.util.*;
+
+public class BinaryNumber {
+        public static List<String> generateBinaryNumbers(int N) {
+            List<String> result = new ArrayList<>();
+            Queue<String> queue = new LinkedList<>();
+
+            queue.add("1");
+
+            for (int i = 0; i < N; i++) {
+                String current = queue.poll();
+                result.add(current);
+
+                queue.add(current + "0");
+                queue.add(current + "1");
+            }
+
+            return result;
+        }
+
+        public static void main(String[] args) {
+            Scanner sc=new Scanner(System.in);
+            System.out.println("Enter the number ");
+            int n=sc.nextInt();
+            System.out.println(generateBinaryNumbers(n));
+        }
+    }
+/*Enter the number
+5
+[1, 10, 11, 100, 101]
+ */
