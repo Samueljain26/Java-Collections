9b86541e865d92d40de4507cc58f7e6be48b8c2f
Samueljain26 <samueljain77@gmail.com>
2025-03-28 13:21:04 +0530

    [Samuel] ADD: Removing duplicates

---
+++ src/main/java/com/bridgelabz/list/RemoveDuplicates.java
@@ -0,0 +1,27 @@
+package com.bridgelabz.list;
+
+import java.util.*;
+
+public class RemoveDuplicates {
+    public static void main(String[] args) {
+    Scanner sc =new Scanner(System.in);
+    Set<Integer> ls = new HashSet<>();
+    System.out.println("Enter the elements || enter 0 to exit writing- ");
+    int n=sc.nextInt();
+    while(n!=0){
+        ls.add(n);
+        n=sc.nextInt();
+    }
+        System.out.print("New list: " +ls);
+    }
+}
+/*Enter the elements || enter 0 to exit writing-
+1
+4
+6
+2
+1
+2
+0
+New list: [1, 2, 4, 6]
+ */
